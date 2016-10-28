a54e6d8fb37b88a0b8eaedec4ad21776a5e717c2
nathants <me@nathants.com>
2016-10-27 22:17:29 -0700

    revert to dns name for ec2 ip

--- aws/ec2.py
+++ aws/ec2.py
@@ -188,10 +188,14 @@ def _name_group(instance):
 
 
 def ip(*tags, first_n=None, last_n=None):
-    return [i.public_ip_address for i in _ls(tags, 'running', first_n, last_n)]
+    return [i.public_dns_name for i in _ls(tags, 'running', first_n, last_n)]
 
 
 def ip_private(*tags, first_n=None, last_n=None):
+    return [i.private_dns_name for i in _ls(tags, 'running', first_n, last_n)]
+
+
+def ipv4_private(*tags, first_n=None, last_n=None):
     return [i.private_ip_address for i in _ls(tags, 'running', first_n, last_n)]
 
 

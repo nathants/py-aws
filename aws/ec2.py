58d0e95fada340188ad4d661a99822f1838b1403
nathants <me@nathants.com>
2017-03-11 21:12:29 -0700

    fstab isnt getting set correctly every time, so forget it

--- aws/ec2.py
+++ aws/ec2.py
@@ -1031,9 +1031,6 @@ sudo mkfs -t ext4 /dev/nvme0n1p1
 sudo mkdir -p /mnt
 sudo mount -o discard /dev/nvme0n1p1 /mnt
 sudo chown -R ubuntu:ubuntu /mnt
-uuid=$(sudo blkid|grep nvme0n1p1|sed -r 's/.* UUID="([^"]+)".*/\1/')
-echo "UUID=$uuid /mnt ext4 defaults,discard 0 0" | sudo tee -a /etc/fstab
-sudo mount -a # verify no errors in fstab
 """
 
 
